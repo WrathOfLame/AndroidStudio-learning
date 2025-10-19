7deef69864c82219e472182a7b7b98857a984031
WrathOfLame <118675328+WrathOfLame@users.noreply.github.com>
2025-10-20 00:03:10 +0200

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -2,11 +2,13 @@ package com.example.konfiguratorzamowieniapizzy;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,19 +18,54 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private String dodatki = "";
+    CheckBox boxSer = findViewById(R.id.ser);
+    CheckBox boxSzynka = findViewById(R.id.szynka);
+    CheckBox boxPieczarki = findViewById(R.id.pieczarki);
+    CheckBox rabat = findViewById(R.id.studentskiRabat);
+    RadioGroup radioGroupRozmiar = findViewById(R.id.radioGroupRozmiar);
+    Button butt = findViewById(R.id.butt);
+    Button butt2 = findViewById(R.id.wyczysc);
+    RadioGroup radioSos = findViewById(R.id.wyborSosu);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
+        rabat.setOnClickListener(v -> {
+            if(rabat.isChecked()){
+                Toast.makeText(this, "Znizka zostala wlaczona", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "Znizka zostala wylaczona", Toast.LENGTH_SHORT).show();
+            }
+        });
+        butt.setOnClickListener(v -> {
+            aktualizujCene();
+        });
+        butt2.setOnClickListener(v -> clear());
+        radioGroupRozmiar.setOnCheckedChangeListener((group, checkedId) -> {
+            aktualizujCene();
+        });
+        boxSer.setOnClickListener(v -> {
+            aktualizujCene();
+        });
+        boxPieczarki.setOnClickListener(v -> {
+            aktualizujCene();
+        });
+        boxSzynka.setOnClickListener(v -> {
+            aktualizujCene();
+        });
+        aktualizujCene();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    public void aktualizujCene(View view){
+    public void aktualizujCene(){
         TextView cenaWynikowa = findViewById(R.id.cenaWynikowa);
         double wynik = obliczCene();
         if(dodatki.equals("")) dodatki = "Brak dodatkow";
@@ -53,10 +90,7 @@ public class MainActivity extends AppCompatActivity {
             wynik = CENA_DUZA;
             dodatki+="Rozmiar duzy ";
         }
-        CheckBox boxSer = findViewById(R.id.ser);
-        CheckBox boxSzynka = findViewById(R.id.szynka);
-        CheckBox boxPieczarki = findViewById(R.id.pieczarki);
-        RadioGroup radioSos = findViewById(R.id.wyborSosu);
+
         int wybranySos = radioSos.getCheckedRadioButtonId();
         if(boxSer.isChecked()){
             wynik+=2.0;
@@ -79,4 +113,12 @@ public class MainActivity extends AppCompatActivity {
         }
         return wynik;
     }
+    private void clear(){
+        dodatki = "";
+        boxPieczarki.setChecked(false);
+        boxSzynka.setChecked(false);
+        boxSer.setChecked(false);
+        radioSos.check(R.id.bezSosu);
+    }
+
 }
