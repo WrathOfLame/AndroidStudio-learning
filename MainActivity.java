cea6d6e2a9b56b006454bea6c7acd5ec27db8e6d
WrathOfLame <118675328+WrathOfLame@users.noreply.github.com>
2025-11-16 22:58:02 +0100

    Refactor MainActivity layout and remove code

--- MainActivity.java
+++ MainActivity.java
@@ -1,45 +1,24 @@
-package com.example.zad3lekcja12;
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:orientation="vertical"
+    android:id="@+id/main"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    tools:context=".MainActivity">
 
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
+    <TextView
+        style="@style/NaglowekEkranu"
+        android:text="Panel Ustawień" />
+    <Switch
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:text="Włącz tryb PRO"
+        android:textSize="18sp"
+        android:checked="true" />
+    <Button
+        style="@style/PrzyciskGlowny"
+        android:text="Zapisz ustawienia" />
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        Button butt1 = findViewById(R.id.butt1);
-        Button butt2 = findViewById(R.id.butt2);
-        Button butt3 = findViewById(R.id.butt3);
-
-        butt1.setOnClickListener(v -> {
-            aktualizujTekst("Klikneto przycisk 1");
-        });
-        butt2.setOnClickListener(v -> {
-            aktualizujTekst("Klikneto przycisk 2");
-        });
-        butt3.setOnClickListener(v -> {
-            aktualizujTekst("Klikneto przycisk 3");
-        });
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
-    private void aktualizujTekst(String wiadomosc){
-        TextView text = findViewById(R.id.mainText);
-        text.setText(wiadomosc);
-    }
-}
+</LinearLayout>
