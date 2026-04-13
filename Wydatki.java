d077f2ae5936aa0cea01236aa1fbb443964f83ee
WrathOfLame <118675328+WrathOfLame@users.noreply.github.com>
2026-04-13 09:00:39 +0200

    Create Wydatek class with properties and constructor

---
+++ Wydatki.java
@@ -0,0 +1,11 @@
+public class Wydatek{
+    private final String nazwa;
+    private final double kwota;
+    private final String kategoria;
+
+    public Wydatek(String nazwa, double kwota, String kategoria){
+        this.nazwa = nazwa;
+        double kwota = kwota;
+        this.kategoria = kategoria;
+    }
+}
