ef03a51ab8a827c8f97f7a63a1b7a4c68a9b4d43
WrathOfLame <118675328+WrathOfLame@users.noreply.github.com>
2026-04-21 21:26:05 +0200

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,6 +1,13 @@
-package com.example.projectmenedzerwydatkow;
+package com.example.menedzerwydatkow;
+
+import static java.lang.Integer.parseInt;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +15,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.UncheckedIOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText poleNazwaWydatku;
+    private EditText poleKwotaWydatku;
+    private Spinner spinnerKategoria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        poleNazwaWydatku = findViewById(R.id.tekstNazwa);
+        poleKwotaWydatku = findViewById(R.id.tekstKwota);
+        spinnerKategoria = findViewById(R.id.tekstKategoria);
+        Button butt = findViewById(R.id.przyciskUsun);
+        butt.setOnClickListener(v -> onClick(v));
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+    @Override
+    public void onClick(View view){
+        String nazwaWydatku = poleNazwaWydatku.getText().toString();
+        String kwotaWydatku = poleKwotaWydatku.getText().toString();
+        String kategoria = spinnerKategoria.getSelectedItem().toString();
+        if(nazwaWydatku.isEmpty() || kwotaWydatku.isEmpty()){
+            Toast.makeText(this, "Fill all fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        double kwotaWydatkuInt = Double.parseDouble(kwotaWydatku);
+        Wydatek w = new Wydatek(nazwaWydatku, kwotaWydatkuInt, kategoria);
+        Toast.makeText(this, "Object created", Toast.LENGTH_SHORT).show();
+    }
+    
 }
